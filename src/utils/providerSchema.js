af0fec5823e5bbc1beab0e8fa111f0725332c982
Adrenine <adrenine13@gmail.com>
2021-05-26 17:40:42 +0530

    minute schema bug fixed

--- src/utils/providerSchema.js
+++ src/utils/providerSchema.js
@@ -4,7 +4,7 @@ const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 export default yup.object().shape({
-  userName: yup.string().required('User Name is Required'),
+  displayName: yup.string().required('User Name is Required'),
   address: yup.string().required('Address is Required'),
   phoneNumber: yup
     .string()
